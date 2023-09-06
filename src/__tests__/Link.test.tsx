240cc75d305a7f8dc2d66c70129629bd7918d923
VladimirKrasov <vladimirkrasov.ex@gmail.com>
2023-09-06 15:57:39 +0300

    Add tests

---
+++ src/__tests__/Link.test.tsx
@@ -0,0 +1,30 @@
+// Тестирование при помощи снимков
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import { Link } from '../components/Link/Link';
+
+it('Изменяет состояние при наведении курсора', () => {
+  const component = renderer.create(
+    <Link page="login">Login</Link>,
+  );
+  // Отрисуем компонент
+  let tree: any = component.toJSON();
+  expect(tree).toMatchSnapshot();
+
+  // Имитируем наведение мышкой
+  renderer.act(() => {
+    tree.props.onMouseEnter();
+  });
+  // Перерисовываем компонент
+  tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+
+  // Имитируем уход мышки
+  renderer.act(() => {
+    tree?.props.onMouseLeave();
+  });
+  // Перерисовываем компонент
+  tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
