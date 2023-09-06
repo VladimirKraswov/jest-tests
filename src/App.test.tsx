2a175b0f1d1fe9c49a73e4ec44a51ae990c0f6b1
VladimirKrasov <vladimirkrasov.ex@gmail.com>
2023-09-06 11:08:35 +0300

    Add easy tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,51 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, fireEvent, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('App component', () => {
+  beforeEach(() => {
+    // Выполнить перед началом тестирования любого it/test блока
+    console.log('Start "it" test');
+  })
+
+  afterEach(() => {
+    // Выполнить после тестирования любого it/test блока
+    console.log('End "it" test');
+  })
+
+  it('Поля ввода электронной почты и пароля отображаются', () => {
+    render(<App />);
+    const emailInput = screen.getByLabelText('Email:');
+    const passwordInput = screen.getByLabelText('Пароль:');
+    expect(emailInput).toBeInTheDocument();
+    expect(passwordInput).toBeInTheDocument();
+  });
+
+  it('Поле email корректно обрабатывает ввод', () => {
+    render(<App />);
+    const emailInput = screen.getByLabelText('Email:');
+    fireEvent.change(emailInput, { target: { value: 'test@example.com' } });
+    expect(emailInput).toHaveValue('test@example.com');
+  });
+
+  it('Поле password корректно обрабатывает ввод', () => {
+    render(<App />);
+    const passwordInput = screen.getByLabelText('Пароль:');
+    fireEvent.change(passwordInput, { target: { value: 'password123' } });
+    expect(passwordInput).toHaveValue('password123');
+  });
+
+  it('Нажатие кнопки регистрации отрабатывает', async () => {
+    render(<App />);
+    const registerButton = screen.getByText('Зарегистрироваться');
+    fireEvent.click(registerButton);
+  });
+
+  it('Линки "Забыли пароль" и "Войти" отображаются', () => {
+    render(<App />);
+    const forgotPasswordLink = screen.getByText('Забыли пароль');
+    const loginLink = screen.getByText('Войти');
+    expect(forgotPasswordLink).toBeInTheDocument();
+    expect(loginLink).toBeInTheDocument();
+  });
+})
